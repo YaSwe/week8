21aef456a730c92ffed325457308cd7f7f5b6d19
ThuYasWe <s10222373@connect.np.edu.sg>
2024-01-27 13:01:46 +0800

    linted

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask
 from account import account_bp
 from capstone import capstone_bp
+from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = 'dop_assg'
+app.secret_key = "dop_assg"
 
 app.register_blueprint(account_bp)
 app.register_blueprint(capstone_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# test
